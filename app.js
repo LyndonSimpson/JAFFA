3632a82fc629a013058f4ba300ceb9b2d9f5e028
LyndonSimpson <lyndon.simpson@gmail.com>
2023-05-06 02:36:14 +0200

    small tweaks

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.post("/generate", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`JAFFA online and listening on port ${PORT}`);
 });
 
 
